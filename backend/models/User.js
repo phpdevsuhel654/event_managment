736794bcad20f7a1fe7702cbe8d1648cfd7b9fba
phpdevsuhel654 <52044071+phpdevsuhel654@users.noreply.github.com>
2024-12-25 21:21:53 +0530

    Login module update

---
+++ backend/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+
+}, { timeseries: true });
+
+module.export = mongoose.model('User', UserSchema);
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Connect to the database
+connectDB();
+
+// Middleware, routes, etc.
+app.use(bodyParser.urlencoded());
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+app.use("/api/auth", require("./routes/auth"));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
