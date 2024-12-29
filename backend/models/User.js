b8819e221d24ddabca202c527bf4d5a17e6b70c2
phpdevsuhel654 <52044071+phpdevsuhel654@users.noreply.github.com>
2024-12-29 20:15:00 +0530

    Added created_at and updated_at timestamps in user table

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,6 +5,11 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, require: true, unique: true },
     password: { type: String, require: true },
 
-}, { timeseries: true });
+});
+
+UserSchema.set('timestamps', {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+})
 
 module.exports = mongoose.model('User', UserSchema);
