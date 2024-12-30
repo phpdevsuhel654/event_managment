675928b9b23ad8da8d831b582253ed58b1dce431
Umesh L <124122450+UmeshLakhani90@users.noreply.github.com>
2024-12-30 21:43:54 +0530

    First commit from umesh

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -18,11 +18,52 @@ const Login = () => {
 
     return (
         <form onSubmit={handleLogin}>
-            <h2>Login</h2>
-            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-            <button type="submit">Login</button>
-        </form>
+        <h3>Sign In</h3>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            value={email} onChange={(e) => setEmail(e.target.value)} required 
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            value={password} onChange={(e) => setPassword(e.target.value)} required
+          />
+        </div>
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Forgot <a href="#">password?</a>
+        </p>
+      </form>
+        // <form onSubmit={handleLogin}>
+        //     <h2>Login</h2>
+        //     <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        //     <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        //     <button type="submit">Login</button>
+        // </form>
     );
 };
 
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const Register = () => {
-    const [name, setName] = useState("");
+    const [name, lastname, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -18,13 +18,58 @@ const Register = () => {
     };
 
     return (
+
         <form onSubmit={handleRegister}>
-            <h2>Register</h2>
-            <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
-            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-            <button type="submit">Register</button>
-        </form>
+        <h3>Sign Up</h3>
+        <div className="mb-3">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+            value={name} onChange={(e) => setName(e.target.value)} required 
+          />
+        </div>
+        <div className="mb-3">
+          <label>Last name</label>
+          <input type="text" className="form-control" placeholder="Last name" 
+          value={lastname} onChange={(e) => setName(e.target.value)} required
+          />
+        </div>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            value={email} onChange={(e) => setEmail(e.target.value)} required
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            value={password} onChange={(e) => setPassword(e.target.value)} required 
+          />
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered <a href="/login">sign in?</a>
+        </p>
+      </form>
+        // <form onSubmit={handleRegister}>
+        //     <h2>Register</h2>
+        //     <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
+        //     <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        //     <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        //     <button type="submit">Register</button>
+        // </form>
     );
 };
 
